cd06c7998c5184ab4cbe077f663a14dd1403e904
racmathafidz <rac.hafidz@gmail.com>
2021-04-21 16:08:34 +0700

    add app page

---
+++ src/pages/appPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class appPage extends Component {
+    render() {
+        return (
+            <div className="w-full h-screen bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500">
+                
+            </div>
+        )
+    }
+}
