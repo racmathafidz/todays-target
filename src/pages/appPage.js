de11ba97730f62e027ab8d35e3614e222ff071db
racmathafidz <rac.hafidz@gmail.com>
2021-05-29 19:08:56 +0700

    add target hooks

--- src/pages/appPage.js
+++ src/pages/appPage.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import DoTarget from 'parts/DoTarget'
 import DoneTarget from 'parts/DoneTarget'
 
 export default function appPage() {
+
+    const [doTargetList, setDoTargetList] = useState([])
+    console.log(doTargetList)
+
+    // Set the body background color
+    useEffect(() => {
+        document.body.className="app-page"
+    })
+
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-10 xl:gap-10 w-full h-screen px-4 py-3 sm:px-10 sm:py-8 xl:px-24 xl:py-10 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-            <DoTarget/>
+        <div className="app-page grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-10 xl:gap-10 w-full px-4 py-3 sm:px-10 sm:py-8 xl:px-24 xl:py-22">
+            <DoTarget doTargetList={doTargetList} setDoTargetList={setDoTargetList}/>
             <DoneTarget/>
         </div>
     )
