9419e85b5c9082e884e7d8d784a17c5a79ef2388
racmathafidz <rac.hafidz@gmail.com>
2021-05-29 19:09:53 +0700

    add submit new target feature

--- src/parts/DoTarget.js
+++ src/parts/DoTarget.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal'
 
 import Button from 'elements/Button'
 import Form from 'elements/Form'
+import List from 'parts/DoTargetList'
 
 const customStyles = {
     content : {
@@ -17,23 +18,55 @@ const customStyles = {
     }
 };
   
-// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement('#root')
 
-export default function DoTarget() {
-    const [modalIsOpen, setModalIsOpen] = useState(false)
+export default function DoTarget({ doTargetList, setDoTargetList }) {
 
-    function openModal() {
+    let value
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [hasError, setHasError] = useState(false)
+    const [addNewTarget, setAddNewTarget] = useState({isCompleted : false})
+
+    const handleTitleChange = event => {
+        setAddNewTarget({
+            ...addNewTarget,
+            [event.target.name]: event.target.value
+        })
+        setHasError(false)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        if (addNewTarget !== undefined && addNewTarget.hasOwnProperty('title') && addNewTarget.title.trim() !== "") {
+    
+            //Add the target
+            const newTarget = [...doTargetList, addNewTarget]
+            setDoTargetList(newTarget)
+
+            //Close modal
+            setHasError(false)
+            closeModal()
+
+        } else {
+            setHasError(true)
+        }
+    }
+
+    const openModal = () => {
         setModalIsOpen(true)
     }
-    function closeModal() {
+
+    const closeModal = () => {
+        setAddNewTarget({isCompleted : false})
+        setHasError(false)
         setModalIsOpen(false)
     }
 
     return (
-        <div className="bg-white px-5 py-1 xl:px-10 xl:py-3 border shadow-lg rounded-2xl">
+        <div className="flex flex-col bg-white px-5 py-1 xl:px-10 xl:py-3 border shadow-lg rounded-2xl">
             <div className="flex items-center justify-between">
-                <dv className="flex items-center">
+                <div className="flex items-center">
                     <h1 className="text-4.5xl text-gray-700">
                         Do
                     </h1>
@@ -41,11 +74,11 @@ export default function DoTarget() {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 sm:h-12 w-10 sm:w-12 text-red-500" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                     </svg>
-                </dv>
+                </div>
             
                 <Button 
                     type="button"
-                    className="focus:outline-none"
+                    className="order-last focus:outline-none"
                     onClick={openModal}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 sm:h-12 w-10 sm:w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -60,43 +93,51 @@ export default function DoTarget() {
                     contentLabel="Add Target Modal"
                 >
                     <h1 className="text-2.5xl text-gray-700 mb-2.5">Add Your Target</h1>
-                    <div className="flex flex-col">
-                        <Form
-                            id="project-title"
-                            name="project-title"
-                            type="text"
-                            
-                            placeholder="Project Title"
-                            className="sm:w-120"
-                        />
-                        <Form
-                            id="project-description"
-                            name="project-description"
-                            type="textarea"
-                            
-                            placeholder="Project Description"
-                            className="sm:w-120"
-                        />
-                    </div>
-                    <div className="flex flex-row-reverse mt-2">
-                        <Button
-                            type="button"
-                            className="w-32 h-11 text-white bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 rounded-xl transform transition duration-500 hover:shadow-lg"
-                            onClick={closeModal}
-                        >
-                            Submit
-                        </Button>
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col">
+                            <Form
+                                id="project-title"
+                                name="title"
+                                type="text"
+                                value={value}
+                                placeholder="Project Title"
+                                className="sm:w-120"
+                                onChange={handleTitleChange}
+                                hasError={hasError}
+                            />
+                            <Form
+                                id="project-description"
+                                name="description"
+                                type="textarea"
+                                value={value}
+                                placeholder="Project Description (Optional)"
+                                className="sm:w-120"
+                                onChange={handleTitleChange}
+                            />
+                        </div>
+                        <div className="flex flex-row-reverse mt-2">
+                            <Button
+                                type="submit"
+                                className="cursor-pointer w-32 h-11 text-white bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 rounded-xl transform transition duration-500 focus:outline-none hover:shadow-lg"                            
+                            >
+                                Submit
+                            </Button>
 
-                        <Button 
-                            type="button"
-                            className="w-24 h-11 text-gray-400 mr-1 focus:outline-none"
-                            onClick={closeModal}
-                        >
-                            Cancel
-                        </Button>
-                    </div>
+                            <Button 
+                                type="button"
+                                className="w-24 h-11 text-gray-400 mr-1 focus:outline-none"
+                                onClick={closeModal}
+                            >
+                                Cancel
+                            </Button>
+                        </div>
+                    </form>
                 </Modal>
             </div>
+
+            <div className="flex">
+                <List targetList={doTargetList} openModal={openModal} />
+            </div>            
         </div>
     )
 }
