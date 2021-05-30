0b97e910f0e04259a3adcbb415757ce76ba757bb
racmathafidz <rac.hafidz@gmail.com>
2021-05-30 13:00:17 +0700

    add delete function and list component

--- src/parts/DoneTarget.js
+++ src/parts/DoneTarget.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-export default function DoneTarget() {
+import List from 'parts/DoTargetList'
+
+export default function DoneTarget({ doneTargetList, setTargetList }) {
+
+    const deleteTarget = index => {
+        const newTarget = [...doneTargetList]
+        newTarget.splice(index, 1)
+        setTargetList(newTarget)
+    }
+
     return (
         <div className="bg-white px-5 py-1 xl:px-10 xl:py-3 border shadow-lg rounded-2xl">
             <div className="flex items-center">
@@ -11,6 +20,10 @@ export default function DoneTarget() {
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                 </svg>
             </div>
+
+            <div className="flex">
+                <List doneTargetList={doneTargetList} deleteDoneTarget={deleteTarget} doneTarget />
+            </div>      
         </div>
     )
 }
