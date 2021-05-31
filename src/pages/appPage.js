f9cefb5d8d81263f9e46de66cfdc561d9924f097
racmathafidz <rac.hafidz@gmail.com>
2021-05-31 11:08:07 +0700

    add local storage

--- src/pages/appPage.js
+++ src/pages/appPage.js
@@ -13,6 +13,23 @@ export default function appPage() {
         document.body.className="app-page"
     })
 
+    // Getting local storage data
+    useEffect(() => {
+        let target
+
+        target = JSON.parse(localStorage.getItem('doTarget'))
+        if ( target !== "" ) setDoTargetList(target)
+
+        target = JSON.parse(localStorage.getItem('doneTarget'))
+        if ( target !== "" ) setDoneTargetList(target)
+    }, [])
+
+    // Setting local storage data
+    useEffect(() => {
+        localStorage.setItem('doTarget', JSON.stringify(doTargetList))
+        localStorage.setItem('doneTarget', JSON.stringify(doneTargetList))
+    }, [doTargetList, doneTargetList])
+
     return (
         <div className="app-page grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-10 xl:gap-10 w-full px-4 py-3 sm:px-10 sm:py-8 xl:px-24 xl:py-22">
             <DoTarget doTargetList={doTargetList} setDoTargetList={setDoTargetList} doneTargetList={doneTargetList} setDoneTargetList={setDoneTargetList} />
